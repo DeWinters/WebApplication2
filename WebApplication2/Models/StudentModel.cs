e03ee035ea87dd19383afca47d14e6b8d9deaf97
DeWinters <derekwwinters@gmail.com>
2016-11-19 21:57:39 +0000

    Add project files.

---
+++ WebApplication2/Models/StudentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;                                      // ?
+using System.Threading.Tasks;                           // ?
+using System.ComponentModel.DataAnnotations;            //
+
+namespace WebApplication2.Models
+{
+    public class StudentModel
+    {
+        [Required(ErrorMessage = "You must enter your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "You must enter your Class")]
+        public string Class { get; set; }
+
+    }
+}
